b05afa2292ff0e05f66037743362efac8794b035
salwaavranitaa <120107615+salwaavranitaa@users.noreply.github.com>
2022-12-08 21:04:49 +0700

    Add files via upload

---
+++ Tugas Akhir Praktikum DAP/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        clifford: '#da373d',
+      },
+      fontFamily: {
+        lora : "Lora, serif"
+      },
+    },
+  },
+  plugins: [],
+}
